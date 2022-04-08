90c1f02488db4d6d3be9ef176c019ce34741da95
lay4play <65734860+lay4play@users.noreply.github.com>
2022-04-08 10:56:25 +0200

    sistemata roba

--- arraycontrario/arraycontrario.c
+++ arraycontrario/arraycontrario.c
@@ -1,26 +1,9 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include <stdlib.h>
-=======
->>>>>>> d7b1888 (arraycontrario)
 #include <time.h>
 #include <stdlib.h>
 
 int main(){
-<<<<<<< HEAD
-    int array[6], array1[6], i, c=0;
-    srand(time(NULL));
-    for(i=0;i<6;i++){
-        array[i]= (int)(rand()%100);
-        printf("%d", array[i]);
-    }
-    for(i=7;i>=0;i=i-1){
-        array1[i] = array[c];
-        c++;
-    }
-    for(i=0;i<6;i++){
-        printf("%d", array1[i]);
-=======
     int array[6], arrayContrario[6], i, c;
     srand(time(NULL));
     for(i=0;i<6;i++){
@@ -33,7 +16,6 @@ int main(){
         arrayContrario[c] = array[i];
         printf("%d ", arrayContrario[c]);
         c++; 
->>>>>>> d7b1888 (arraycontrario)
     }
     printf("\n"); //reset cursor to normal position
     return 0;
