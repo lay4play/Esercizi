c1e633df3c371f0ed50f2329e6f55bf2154dc752
alessio scardigno <65734860+lay4play@users.noreply.github.com>
2023-01-24 11:12:01 +0100

    aggiornamento

--- Teoria/acquisizioneStringhe.cpp
+++ Teoria/acquisizioneStringhe.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
-#define MAXN 20
+#define MAXN 20                         //comandi mandati direttamente al compilatore
+#define MAXS 80
 
 using namespace std;
 
@@ -10,13 +11,12 @@ void minuscolo(char []);
 int main(){
     char messaggio[MAXN];
     string messaggio1;
-    string aux1;
     int num;
     cout << "Inserire il numero di alunni" << endl;
     cin >> num;
-//    getline(cin, aux1);
-    fflush(stdin);
+    fflush(stdin);                          //Pulise il buffer della tastiera, instassi C
     cout << "Inserire nome" << endl;
+    //cin.ignore(MAXS,'\n');                //Acquisizione stringhe C++ senza svuotare il buffer ma ignorando lo spazio, sintassi C++
     getline(cin, messaggio1);             //acquisizione stringhe C++
     //cin.getline(messaggio, MAXN, '\n');    //acquisizione stringhe C utilizzabile solo in C++
     //gets(messaggio);                      //acquisizione stringhe C
